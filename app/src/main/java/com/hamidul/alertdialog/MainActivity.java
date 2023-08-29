2df818fa168a0c35b13c3a672eb9c7153121a752
user <practicecoddin@gmail.com>
2023-08-29 20:14:38 +0600

    AlertDialog

--- app/src/main/java/com/hamidul/alertdialog/MainActivity.java
+++ app/src/main/java/com/hamidul/alertdialog/MainActivity.java
@@ -20,35 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-
-//        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog,null);
-//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
-//
-//        alertDialog.setView(view);
-//
-//        btnNo = (Button) view.findViewById(R.id.no);
-//        btnYes = (Button) view.findViewById(R.id.yes);
-//
-//        final AlertDialog dialog = alertDialog.create();
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//                dialog.show();
-//            }
-//        });
-//
-//        btnNo.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                dialog.cancel();
-//                Toast.makeText(MainActivity.this, "No", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-
 
     }
 
